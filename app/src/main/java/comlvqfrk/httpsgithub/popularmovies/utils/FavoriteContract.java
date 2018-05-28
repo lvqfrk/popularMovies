70e24e276090893175a72250283216d6a34f6219
lvqfrk <lev.franck@gmail.com>
2018-05-28 20:06:33 +0200

    create favorite contract and db helper

---
+++ app/src/main/java/comlvqfrk/httpsgithub/popularmovies/utils/FavoriteContract.java
@@ -0,0 +1,49 @@
+package comlvqfrk.httpsgithub.popularmovies.utils;
+
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+public class FavoriteContract {
+
+    public static final String CONTENT_AUTHORITY = "comlvqfrk.httpsgithub.popularmovies";
+
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    /* define the content of the favorites table */
+    public static final class FavoriteEntry implements BaseColumns {
+        /* name of the table used by the database */
+        public static final String TABLE_NAME = "favorites";
+
+        /* name of the column to store id's of movies */
+        public static final String COLUMN_TMDB_ID = "tmdb_id";
+        /* name of the column to store the title*/
+        public static final String COLUMN_TITLE = "title";
+        /* name of the column to store the poster */
+        public static final String COLUMN_POSTER = "poster";
+        /* name of the column to store the release date */
+        public static final String COLUMN_RELEASE_DATE = "release_date";
+        /* name of the column to store the rates average */
+        public static final String COLUMN_RATE = "rate";
+        /* name of the column to store the overview of the movie*/
+        public static final String COLUMN_OVERVIEW = "overview";
+
+        /* The base CONTENT_URI used to query the favorite table from the content provider */
+        public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon()
+                .appendPath(TABLE_NAME)
+                .build();
+
+        /**
+         * Builds a URI that adds the movies _ID to the end of the favorite content URI path.
+         * This is used to query details about a single favorite entry by _ID. This is what we
+         * use for the detail view query.
+         *
+         * @param id Unique id pointing to that row
+         * @return Uri to query details about a single movie entry
+         */
+        public static Uri buildTodoUriWithId(long id) {
+            return CONTENT_URI.buildUpon()
+                    .appendPath(Long.toString(id))
+                    .build();
+        }
+    }
+}
---
+++ app/src/main/java/comlvqfrk/httpsgithub/popularmovies/utils/FavoriteDbHelper.java
@@ -0,0 +1,38 @@
+package comlvqfrk.httpsgithub.popularmovies.utils;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class FavoriteDbHelper extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "favorites.db";
+
+    private static final int DATABASE_VERSION = 1;
+
+    public FavoriteDbHelper (Context context) {
+        super(context, DATABASE_NAME,null ,DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        final String SQL_CREATE_FAVORITES_TABLE =
+
+                "CREATE TABLE " + FavoriteContract.FavoriteEntry.TABLE_NAME + " (" +
+                        FavoriteContract.FavoriteEntry._ID + "INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                        FavoriteContract.FavoriteEntry.COLUMN_TMDB_ID + "INTEGER NOT NULL, " +
+                        FavoriteContract.FavoriteEntry.COLUMN_TITLE + "TEXT NOT NULL, " +
+                        FavoriteContract.FavoriteEntry.COLUMN_POSTER + "BLOB, " +
+                        FavoriteContract.FavoriteEntry.COLUMN_RELEASE_DATE + "TEXT, " +
+                        FavoriteContract.FavoriteEntry.COLUMN_RATE + "REAL, " +
+                        FavoriteContract.FavoriteEntry.COLUMN_OVERVIEW + "TEXT);";
+
+        db.execSQL(SQL_CREATE_FAVORITES_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + FavoriteContract.FavoriteEntry.TABLE_NAME);
+        onCreate(db);
+    }
+}
